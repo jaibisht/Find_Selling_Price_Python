cb0a6bb8017c538152ee41e9cc2f28ef4df309fe
Jai Singh Bisht <79037585+jaibisht@users.noreply.github.com>
2022-10-25 10:23:05 +0530

    Add files via upload

---
+++ sellingprice.py
@@ -0,0 +1,4 @@
+cost_price = int(input("Enter the cost price of object:"))
+profit = int(input("Enter the Profit made:"))
+selling_price = cost_price + profit
+print("The selling price is ", selling_price)
